c64d391381a7427a29d8a0a9242a43a7cf777e1b
Zsolt Paroczi <Zsolt.Paroczi@t-systems.com>
2023-11-08 12:20:00 +0100

    init

---
+++ iac/create-vms4docker.ps1
@@ -0,0 +1,35 @@
+$susbscription = 'a8f77654-9254-4d5f-9c61-712b81de23c6'
+$resourceGroup = 'oe-docker-rg'
+$vmName = 'oe-docker-vm'
+$adminUser = 'azureadm'
+$adminPwd = 'Azureadm1234.'
+$vnet = 'oe-docker-vnet'
+$subnet = 'subnet-1'
+$count = 2
+
+az account set --subscription $susbscription
+
+
+az group create --location westeurope --resource-group $resourceGroup
+
+az network vnet create `
+    --name oe-docker-vnet `
+    --resource-group $resourceGroup `
+    --address-prefix 10.0.0.0/16 `
+    --subnet-name subnet-1 `
+    --subnet-prefixes 10.0.0.0/24
+
+az vm create `
+    --resource-group $resourceGroup `
+    --name $vmName `
+    --image Ubuntu2204  `
+    --public-ip-sku Standard `
+    --admin-username $adminUser `
+    --admin-password $adminPwd `
+    --vnet-name $vnet `
+    --subnet $subnet `
+    --count $count
+    
+
+az group delete --resource-group $reysourceGroup
+
