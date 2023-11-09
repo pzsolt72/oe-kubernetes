5a067c8fd4ede5c70f175960ee0c978cb5df0564
Zsolt Paroczi <Zsolt.Paroczi@t-systems.com>
2023-11-09 14:30:03 +0100

    hida secrets

--- iac/docker/create-vms4docker.ps1
+++ iac/docker/create-vms4docker.ps1
@@ -1,9 +1,16 @@
-$susbscription = 'a8f77654-9254-4d5f-9c61-712b81de23c6'
+$file_content = Get-Content "./.secret" -raw
+$file_content = [Regex]::Escape($file_content)
+$file_content = $file_content -replace "(\\r)?\\n", [Environment]::NewLine
+$configuration = ConvertFrom-StringData($file_content)
+
+
+
+$susbscription = $configuration.'subscriptionId'
 $resourceGroup = 'oe-docker-rg'
 $vmName = 'oe-docker-vm'
 $vmSize= 'Standard_DS1_v2'  #Standard_DS1_v2 Standard_D2as_v4
 $adminUser = 'azureadm'
-$adminPwd = 'Azureadm1234.'
+$adminPwd = $configuration.'vmAdminPassword'
 $vnet = 'oe-docker-vnet'
 $subnet = 'subnet-1'
 $count = 2
@@ -40,7 +47,6 @@ for ($i = 0; $i -lt $count; $i++) {
         --publisher Microsoft.Azure.Extensions `
         --version 2.0 `
         --settings ./iac/docker/custom-script-config.json
-
    
 }
 
